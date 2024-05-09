9397dcb06843839285cae9cc8a1abf859c0656b1
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-05-09 22:25:50 +0530

    Time: 2 ms (97.53%) | Memory: 44.9 MB (24.12%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
@@ -7,10 +7,7 @@ class Solution {
             if(nums[i]>0) pos[nums[i]]++;
             else neg[-1*nums[i]]++;      
         }
-        System.out.println("PRINING");
-        for(int i:pos) if(i>0)System.out.print(i + " ");
-        System.out.println();
-        for(int i:neg) if(i>0) System.out.print(i + " ");
+         
         for(int i=n-1;i>=1;i--){
             if(pos[i]>0 && neg[i]>0) return i;
         }
