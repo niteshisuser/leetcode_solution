eb7a668eb05176028cfc1370d9a11900330a4238
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-05-10 22:19:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (92.76%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -10,20 +10,11 @@ class Solution {
  
         /* If arr[l...mid] first subarray is sorted */
         if (arr[l] <= arr[mid]) {
-            /* As this subarray is sorted, we
-               can quickly check if key lies in
-               half or other half */
             if (key >= arr[l] && key <= arr[mid])
                 return search(arr, l, mid - 1, key);
-            /*If key not lies in first half subarray,
-           Divide other half  into two subarrays,
-           such that we can quickly check if key lies
-           in other half */
             return search(arr, mid + 1, h, key);
         }
  
-        /* If arr[l..mid] first subarray is not sorted,
-           then arr[mid... h] must be sorted subarray*/
         if (key >= arr[mid] && key <= arr[h])
             return search(arr, mid + 1, h, key);
  
