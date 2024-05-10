9f72da5fef4c719af7a8ae8caa519517235fe603
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-05-11 00:10:25 +0530

    Time: 5 ms (97.42%) | Memory: 56.6 MB (33.22%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        /*
+        -5 -4 -3 -2 -1 0 1 2 3 4 5 
+
+        */
+        int n =  50001;
+        int pos[] = new int[n];
+        int neg[] = new int[n];
+        for(int i:nums){
+            if(i>=0) pos[i]++;
+            else neg[i*-1]++;
+        }
+        int total=nums.length;
+        int ans[]  = new int[total];
+        int index = 0;
+        for(int i=n-1;i>=0;i--){
+            if(neg[i]>0)
+                for(int jj = 0;jj<neg[i];jj++)
+                    ans[index++] = i*-1;
+        }
+        for(int i=0;i<n;i++){
+            if(pos[i]>0) 
+            for(int jj = 0;jj<pos[i];jj++)
+                ans[index++] = i;
+        }
+        return ans;
+    }
+}
