83d9055571d6599cbdaca88fdeae227ac8d99c47
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-06-01 14:08:12 +0530

    Time: 2 ms (86.11%) | Memory: 41.4 MB (83.29%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,16 +1,13 @@
 class Solution {
     public int calPoints(String[] operations) {
         List<Integer> ar = new ArrayList();
-        int temp = 0;
         for(String s:operations){
             switch(s){
-                case "D": temp = ar.get(ar.size()-1)*2;  ar.add(temp); break;
+                case "D":ar.add(ar.get(ar.size()-1)*2); break;
                 case "C": ar.remove(ar.size()-1); break;
-                case "+": temp = ar.get(ar.size()-1) + ar.get(ar.size()-2); ar.add(temp); break;
+                case "+": ar.add(ar.get(ar.size()-1) + ar.get(ar.size()-2)); break;
                 default: ar.add(Integer.parseInt(s)); break;
             }
-            //System.out.println(ar+" and operation is " + s);
-            
         }
         int sum = 0;
             for(int i:ar) sum +=i;
