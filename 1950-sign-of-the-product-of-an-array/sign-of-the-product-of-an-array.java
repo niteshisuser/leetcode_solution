541c25959abcb2b3d62b4844bbb20b3064937796
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-06-07 17:58:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (93.88%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i:nums){
             if(i<0) negCount++;
             if(i==0) return 0;
-            if(i>0) pos=true;
+            //if(i>0) pos=true;
         }
         if(negCount%2!=0) return -1;
         return 1;
