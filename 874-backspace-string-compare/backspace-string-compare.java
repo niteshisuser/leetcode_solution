f8f3df4efccb1e2413cc1e849eddac064597e89f
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-06-08 00:42:05 +0530

    Time: 2 ms (56.92%) | Memory: 41.6 MB (42.97%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -5,14 +5,14 @@ class Solution {
             if(k=='#' && !stack.isEmpty()) stack.pop();
             else if(k!='#') stack.push(k);
         }
-        System.out.println("Stack1"+stack);
+        //System.out.println("Stack1"+stack);
         StringBuilder sb = new StringBuilder();
         while(!stack.isEmpty()) sb.append(stack.pop());
         for(char k:t.toCharArray()){
             if(k=='#' && !stack.isEmpty()) stack.pop();
             else if(k!='#') stack.push(k);
         }
-        System.out.println("Stack2"+stack);
+        //System.out.println("Stack2"+stack);
         StringBuilder sbt = new StringBuilder();
         while(!stack.isEmpty()) sbt.append(stack.pop());
         return sb.toString().equals(sbt.toString());
