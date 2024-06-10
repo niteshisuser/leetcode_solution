6dc50a8df1ea16e70076a9f10b16fdd9cecd745d
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-06-10 11:20:03 +0530

    Time: 55 ms (5.04%) | Memory: 55 MB (7.08%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -1,12 +1,11 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
-        String ans[] = new String[n+1];
-        int index = 0;
-        for(int i=0;i<=n;i++) ans[i]=(i+"");
-        Arrays.sort(ans);
-        List<Integer> aa = new ArrayList();
-        for(String i:ans) if(!i.equals("0")) aa.add(Integer.parseInt(i));
-        return aa;
+        Set<String> set = new TreeSet();
+        Set<Integer> s = new LinkedHashSet();
+        for(int i=1;i<=n;i++) set.add(""+i);
+        for(String ss:set)
+            s.add(Integer.parseInt(ss));
+        return new ArrayList(s);
         
 
     }
