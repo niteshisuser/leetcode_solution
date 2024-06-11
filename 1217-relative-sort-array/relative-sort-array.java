5e9f99294745f83eb030d745dd572a349cb90d43
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-06-11 11:43:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (97.97%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,20 +1,23 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        Map<Integer,Integer> map = new TreeMap();
-        for(int i:arr1) map.put(i,map.containsKey(i)?1+map.get(i):1);
+        int map[] = new int[1001];
+        for(int i:arr1) map[i]++;
         int index = 0;
         for(int i:arr2){
-            if(map.containsKey(i)){
-                for(int j=0;j<map.get(i);j++){
+            if(map[i]>0){
+                for(int j=0;j<map[i];j++){
                     arr1[index++] = i;
                 }
             }
-            map.remove(i);
+            //Reset to zero
+            map[i]=0;
         }
-        for(int i:map.keySet()){
-            for(int j=0;j<map.get(i);j++){
+        for(int i=0;i<=1000;i++){
+            if(map[i]>0){
+                for(int j=0;j<map[i];j++){
                     arr1[index++] = i;
                 }
+            }
         }
         return arr1;
     }
