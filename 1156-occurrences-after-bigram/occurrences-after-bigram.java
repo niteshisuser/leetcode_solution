a1e663333750c94690768bddda9e1124890a8409
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-06-11 12:37:33 +0530

    Time: 2 ms (8.42%) | Memory: 41.5 MB (48.91%) - LeetSync

--- 1156-occurrences-after-bigram/occurrences-after-bigram.java
+++ 1156-occurrences-after-bigram/occurrences-after-bigram.java
@@ -5,10 +5,6 @@ class Solution {
         for(int i=0;i<s.length-2;i++){
             if(s[i].equals(first) &&s[i+1].equals(second)) ans.add(s[i+2]);
         }
-        int nn = ans.size();
-        String [] kb = new String[nn];
-        int index = 0;
-        for(String i:ans) kb[index++]=i;
-        return kb;
+        return ans.toArray(new String[0]);
     }
 }
