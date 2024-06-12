116a0d409a82d45d9fbeafe9e21c5bca80e8a5d7
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-06-12 11:53:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (84.63%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hammingDistance(int n1, int n2) {
+        int x = n1 ^ n2;
+    int setBits = 0;
+ 
+    while (x > 0) 
+    {
+        setBits += x & 1;
+        x >>= 1;
+    }
+ 
+    return setBits;
+    }
+}
