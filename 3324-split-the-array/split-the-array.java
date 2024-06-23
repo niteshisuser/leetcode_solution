3156b3af5951f17d38374f191ac53564cca5f9cb
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-06-23 07:50:52 +0530

    Time: 1 ms (96.13%) | Memory: 43.1 MB (13.17%) - LeetSync

--- 3324-split-the-array/split-the-array.java
+++ 3324-split-the-array/split-the-array.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean isPossibleToSplit(int[] nums) {
-        Map<Integer,Integer> map = new HashMap();
-        for(int i:nums) map.put(i,map.containsKey(i)?1+map.get(i):1);
-        int cEven = 0,cOdd=0;
-        for(int i:map.keySet()){
-            if(map.get(i)>2) return false;
-            else cOdd++;
+        int []map = new int[101];
+        for(int i:nums) map[i]++;
+        for(int i:map){
+            if(i>2) return false;
         }
         return true;
     }
