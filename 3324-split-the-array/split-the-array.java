a716b7b0364a52cea3a59e30b10bf9223ccd2269
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-06-23 07:51:41 +0530

    Time: 1 ms (96.13%) | Memory: 42.8 MB (28.56%) - LeetSync

--- 3324-split-the-array/split-the-array.java
+++ 3324-split-the-array/split-the-array.java
@@ -1,10 +1,13 @@
 class Solution {
     public boolean isPossibleToSplit(int[] nums) {
         int []map = new int[101];
-        for(int i:nums) map[i]++;
-        for(int i:map){
-            if(i>2) return false;
+        for(int i:nums) {
+            map[i]++;
+            if(map[i]>2) return false;
         }
+
+        for(int i:map)
+            if(i>2) return false;
         return true;
     }
 }
