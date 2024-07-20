99aec671254eeab48fb5518270dab0340c570ad9
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-07-20 10:22:09 +0530

    Time: 7 ms (52.59%) | Memory: 53.2 MB (16.33%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
@@ -5,13 +5,7 @@ class Solution {
         int [][]matrix = new int[n][m];
         for(int i = 0; i < n; i ++){
             for(int j = 0; j < m; j ++){
-                if(rowSum[i] < colSum[j]){
-                    matrix[i][j] = rowSum[i];
-                }
-                
-                else{
-                    matrix[i][j] = colSum[j];
-                }
+                matrix[i][j] = rowSum[i] < colSum[j]?rowSum[i]:colSum[j];
                 rowSum[i] -= matrix[i][j];
                 colSum[j] -= matrix[i][j];
             }
