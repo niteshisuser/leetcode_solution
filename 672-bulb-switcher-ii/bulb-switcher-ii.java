bf9ed0efae0d1ded6a9400d862eb7ed87131a643
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-09-18 21:17:51 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (19.31%) - LeetSync

---
+++ 672-bulb-switcher-ii/bulb-switcher-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int flipLights(int n, int k) {
+        if(k == 0) return 1;
+        if(n == 1) return 2;
+        if(n == 2 && k == 1) return 3;
+        if(n == 2 || k == 1) return 4;
+        if(k == 2) return 7;
+        return 8;
+    }
+}
