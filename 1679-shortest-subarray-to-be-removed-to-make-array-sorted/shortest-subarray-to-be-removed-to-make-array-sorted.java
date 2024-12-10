223a27c67b2ed551f6828b7be211b10ac0548f57
NITESH KUMAR GUPTA <32892871+niteshisuser@users.noreply.github.com>
2024-12-10 22:30:45 +0530

    Time: 2 ms (79.38%) | Memory: 64.9 MB (59.74%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.java
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.java
@@ -1,22 +1,34 @@
 class Solution {
-
     public int findLengthOfShortestSubarray(int[] arr) {
-        int right = arr.length - 1;
-        while (right > 0 && arr[right] >= arr[right - 1]) {
+        int n = arr.length;
+
+        //find the longest increasing prefix
+        int left = 0;
+        while(left < n-1 && arr[left] <= arr[left+1]){
+            left++;
+        }
+
+        if(left == n-1){
+            return 0;
+        }
+
+        //find the longest increasing suffix
+        int right = n-1;
+        while(right > 0 && arr[right-1] <= arr[right]){
             right--;
         }
 
-        int ans = right;
-        int left = 0;
-        while (left < right && (left == 0 || arr[left - 1] <= arr[left])) {
-            // find next valid number after arr[left]
-            while (right < arr.length && arr[left] > arr[right]) {
-                right++;
+        int result = Math.min(right, n - left - 1);
+
+        int i = 0, j = right;
+        while(i <= left){
+            while(j < n && arr[i] > arr[j]){
+                j++;
             }
-            // save length of removed subarray
-            ans = Math.min(ans, right - left - 1);
-            left++;
+
+            result = Math.min(result, j-i-1);
+            i++;
         }
-        return ans;
+        return result;
     }
 }
